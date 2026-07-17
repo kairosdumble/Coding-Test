4808bc77bbb46c26c04dac3fb88ced0a3b87de45
Aryeong Yun <142221492+kairosdumble@users.noreply.github.com>
2026-07-17 16:50:03 +0900

    [level 1] Title: 과일 장수, Time: 20.05 ms, Memory: 35 MB -BaekjoonHub

--- C++/프로그래머스/1/135808. 과일 장수/과일 장수.cpp
+++ C++/프로그래머스/1/135808. 과일 장수/과일 장수.cpp
@@ -8,12 +8,8 @@ int solution(int k, int m, vector<int> score) {
     
     sort(score.begin(), score.end(), greater<int>()); // 올림차순으로 정렬
     
-    for(int i =0;i<score.size()/m;i++){ // 사과 m개가 든 상자별로 확인
-        int min = k;                    // 현재의 최솟값 = 가능한 최댓값
-        for(int j = 0 ; j < m ;j ++){   // 상자안에 존재하는 사과들 모두 조회
-            if(min > score[i * m + j]) min = score[i*m +j];
-        }
-        result += min * m;              // 상자 1개 내부에서 최저값 * 사과 갯수
+    for(int i =1 ; i<=score.size()/m ; i++){ // 사과 m개가 든 상자별로 확인
+        result += score[i*m-1]* m;              // 상자 1개 내부에서 최저값 * 사과 갯수
     }
     return result;
 }
