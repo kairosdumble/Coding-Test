58509e4211cf68682132d61dce941f134827ca6f
Aryeong Yun <142221492+kairosdumble@users.noreply.github.com>
2026-07-23 22:58:40 +0900

    [level 3] Title: 길 찾기 게임, Time: 11.81 ms, Memory: 9.64 MB -BaekjoonHub

--- C++/프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.cpp
+++ C++/프로그래머스/3/42892. 길 찾기 게임/길 찾기 게임.cpp
@@ -3,48 +3,47 @@
 #include <algorithm>
 #include <iostream>
 using namespace std;
-struct node {
+struct node{
     int x;
     int y;
     int n;
 };
-
-struct tree {
+struct tree{
     node head;
-    tree*l;
-    tree*r;
+    tree* l;
+    tree* r;
 };
 
-bool compare(node a, node b){
+bool compare(node a,node b){
     if(a.y == b.y){
         return a.x < b.x;
     }else{
-        return a.y >b.y;
+        return a.y > b.y;
     }
 }
 
-void add_tree(tree* t, tree* tmp){ // t에다가 tmp를 붙인다.
+void add_tree(tree*t, tree*tmp){
     if(t->head.x > tmp->head.x){
-        if(t->l != NULL){
+        if(t->l == NULL){
+            t->l = tmp;
+        }else{
             add_tree(t->l,tmp);
         }
-        else{
-            t->l = tmp;
-        }
     }else{
-        if(t->r != NULL){
-            add_tree(t->r,tmp);
-        }else{
+        if(t->r == NULL){
             t->r = tmp;
+        }else{
+            add_tree(t->r,tmp);
         }
     }
 }
 
-vector<int> pre_arr;
-vector<int> post_arr;
+vector <int> pre_arr;
+vector <int> post_arr;
 
 void preSearch(tree* t){
     if(t == NULL) return;
+    
     pre_arr.push_back(t->head.n);
     preSearch(t->l);
     preSearch(t->r);
@@ -52,37 +51,38 @@ void preSearch(tree* t){
 
 void postSearch(tree* t){
     if(t == NULL) return;
+    
     postSearch(t->l);
     postSearch(t->r);
     post_arr.push_back(t->head.n);
 }
 
+
 vector<vector<int>> solution(vector<vector<int>> nodeinfo) {
-    vector<vector<int>> answer; // 정답 제출용
-    vector <node> n; // 정렬용 node 배열
+    vector<vector<int>> answer;
+    vector<node> n;
     
-    for(int i = 0 ; i < nodeinfo.size(); i++){
+    for(int i = 0; i < nodeinfo.size(); i ++){
         node tmp;
         tmp.x = nodeinfo[i][0];
         tmp.y = nodeinfo[i][1];
-        tmp.n = i + 1 ;
+        tmp.n = i+1;
         n.push_back(tmp);
     }
     stable_sort(n.begin(),n.end(),compare);
     
     tree* t = new tree;
-    t-> r = NULL;
-    t-> l = NULL;
     t->head = n[0];
+    t->l = NULL;
+    t->r = NULL;
     
-    for(int i = 1; i < n.size() ; i ++){
+    for(int i = 1 ; i < n.size(); i ++){
         tree* tmp = new tree;
         tmp->head = n[i];
-        tmp->r = NULL;
         tmp->l = NULL;
+        tmp->r = NULL;
         add_tree(t,tmp);
     }
-
     preSearch(t);
     postSearch(t);
     answer.push_back(pre_arr);
